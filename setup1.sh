9c98183c5ac37bb14993bac7bb5359f0420400e2
ZackFra <zack11frazier22@gmail.com>
2020-03-14 17:04:12 +0000

    added express generator

--- setup1.sh
+++ setup1.sh
@@ -14,7 +14,7 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 # install mongo shell ref: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get install -y mongodb-org
+sudo apt install -y mongodb-org
 
 # update + cleanup
 sudo apt update
--- setup2.sh
+++ setup2.sh
@@ -2,5 +2,6 @@
 
 # install node/ npm via nvm
 nvm install node
+npm install -g express-generator
 
 echo 'Done.'
