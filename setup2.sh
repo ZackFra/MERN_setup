982c0e499386a8db4c62b417f69b33f880ef1b5d
ZackFra <zack11frazier22@gmail.com>
2020-03-20 18:08:19 +0000

    update

--- setup2.sh
+++ setup2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# install node/ npm via nvm
-nvm install node
-
 # install create-react-app tool
 npm i create-react-app -g
+
+# install express
+npm i express -g
+
+# install nodemon
+npm i nodemon -g
 
 echo 'Done.'
